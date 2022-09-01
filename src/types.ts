7a8f2974759fdb86067c822d2f9d7c13afbc522d
Wenfeng Wang <kalot.wang@gmail.com>
2022-09-01 12:49:19 +0800

    update types

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,33 @@ export type Option<T> = T | null
 // Potentially should use MultiLocation
 export type Chain = string
 
-export interface Dex {
+export interface IBridgePair {
+  token0: IToken
+  token1: IToken
+
+  /*********** Methods declaration**********/
+  // Return bridge capacity of the given token0
+  getToken0Capcity: () => Option<string>
+  // Return bridge capacity of the given token1
+  getToken1Capcity: () => Option<string>
+}
+
+export interface IBridge {
+  // Source chain
+  chain0: Chain
+  // Corresponding dest chain
+  chain1: Chain
+  // Crosschain assets pair list
+  pairs: IBridgePair[]
+
+  /*********** Methods declaration**********/
+  // Add an crosschain asset pair
+  addBridgePair: (pair: IBridgePair) => void
+  // Return corresponding bridged asset for a given asset
+  getBridgedAsset: (token: IToken) => Option<[Chain, IToken]>
+}
+
+export interface TDex {
   // Name of the DEX
   name: string
   // The chain that DEX deployed on
@@ -14,28 +40,28 @@ export interface Dex {
   // GraphQL indexer api
   indexer: Option<string>
   // Trading pair list
-  pairs: Pair[]
+  pairs: IPair[]
 
   /*********** Methods declaration**********/
   // Return trading pair list that including the specific token
-  getTokenPairs: (token: string) => Pair[]
+  getTokenPairs: (token: string) => IPair[]
 }
 
-export interface Pair {
+export interface IPair {
   // Address or other representation of a trading pair, should be the only indentification of the pair
   id: string
   // Base trading token
-  token0: Token
+  token0: IToken
   // Quote trading token
-  token1: Token
+  token1: IToken
   // Liquidity(balance of pair address) of base trading token
   reserve0: string
   // Liquidity(balance of pair address) of quote trading token
   reserve1: string
   // Capacity of token0, represented by USD
-  capcity0: string
+  capcity0: Option<string>
   // Capacity of token1, represented by USD
-  capcity1: string
+  capcity1: Option<string>
   // Potential swap fee
   swapFee: Option<string>
   // Potential Dev fee
@@ -46,13 +72,15 @@ export interface Pair {
   getReserves: () => [string, string]
   // Fetch latest reserve data from indexer or blockchain, and return it
   updateReserves: () => [string, string]
-  // Perspectively return token prices represented by USD, dynamic fetch&calculate from blockchain
-  getPrices: () => [string, string]
+  // Perspectively return token prices represented by USD, dynamic fetch&calculate from blockchain or other 3rd parties
+  getPrices: () => [Option<string>, Option<string>]
   // Return total fee paid according to current trading status, dynamic fetch&calculate from blockchain
-  getFee: (token: Token) => string
+  getFee: (token: IToken) => Option<string>
+  // Perspectively return capcity of token0 and token1, dynamic fetch&calculate from blockchain or other 3rd parties
+  getCapcities: () => [Option<string>, Option<string>]
 }
 
-export interface Token {
+export interface IToken {
   // Address or a location of the token, should be the only indentification of the asset
   id: string
   // Name of the asset
